14cdc7e76b8bc318dbd679d8e1c02903b5828d4e
LilDavtyan <lilitdavtyan01@gmail.com>
2016-08-24 23:56:05 +0400

    commit

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        public static bool AreStringsAnagrams(string a, string b)
+        {
+            if (a == b)
+                return true;
+            int count;
+            List<string> a1 = new List<string>();
+            List<string> b1 = new List<string>();
+            if (a.Length == b.Length)
+            {
+                for (int i = 0; i < a.Length; i++)
+                {
+                    a1.Add(a[i].ToString());
+                }
+
+                for (int i = 0; i < b.Length; i++)
+                {
+                    b1.Add(b[i].ToString());
+                }
+
+               
+                foreach (var item1 in a1)
+                {
+                    count = 0;
+                    foreach (var item2 in b1)
+                    {
+                        if (item1 == item2)
+                        {
+                            count++;
+                            b1.Remove(item2);
+                            break;
+                        }
+                                            
+                    }
+                   if (count==0)
+                   {
+                       return false;
+                   }
+                    
+                }
+                return true;
+            }
+            else return false;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AreStringsAnagrams("11111", "11111").ToString());
+           Console.ReadLine();           
+
+        }
+    }
+}
